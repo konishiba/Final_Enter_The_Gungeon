2bf884ebe31281697ffeb8c15c757852067e6aef
konishiba <yoan.gimenez1202@gmail.com>
2025-05-13 20:44:04 +0200

    htrd

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include "Engine.h"
 #include "LevelManager.h"
+#include "Proto_Map.h"
 
 
 void InitConfig()
@@ -13,8 +14,8 @@ int main()
 {
 	InitConfig();
 
-	//TESTLevel* _level = new TESTLevel("TESTLevel");
-	//M_LEVEL.SetLevel(_level);
+	Proto_Map* _level = new Proto_Map("mapTest");
+	M_LEVEL.SetLevel(_level);
 
 	Engine _engine;
 	_engine.Start();
---
+++ Proto_Map.cpp
@@ -0,0 +1,42 @@
+#include "Proto_Map.h"
+//#include "PlayerPawn.h"
+#include "Room.h"
+#include "CircleActor.h"
+#include "CUSTOMGameMode.h"
+#include "CanvasWidget.h"
+
+Proto_Map::Proto_Map(const string& _name) : Level(_name)
+{
+}
+
+void Proto_Map::InitLevel()
+{
+	Super::InitLevel();
+
+	HUD* _hud = GetGameMode()->GetHUD();
+
+	UI::CanvasWidget* canvas = _hud->SpawnWidget<UI::CanvasWidget>();
+	//canvas->SetSize(CAST(Vector2f, GetWindowSize()));
+
+	//ImageWidget* _image = _hud->SpawnWidget<ImageWidget>(CircleShapeData(50.0f));
+	//canvas->AddChild(_image);
+
+	//AActor* _actor = SpawnActor<ACircleActor>(50.0f);
+	//_actor->SetPosition({ 500.0f,500.0f });
+	Room* _room = SpawnActor<Room>();
+	_room->SetPosition({ 10.0f,10.0f });
+
+	//_actor->SetPosition(GetWindowSize() / 2.0f);
+
+	//GetGameMode()->GetHUD()->AddToViewport(canvas);
+}
+
+void Proto_Map::Load()
+{
+	Super::Load();
+}
+
+void Proto_Map::Unload()
+{
+	Super::Unload();
+}
